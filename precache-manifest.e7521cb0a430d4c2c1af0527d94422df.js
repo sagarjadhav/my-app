6012c990d63febf3aa334a3299d7857d767e06e4
Sagar Jadhav <sagar@buddyboss.com>
2019-02-23 13:07:09 +0530

    Updates

---
+++ precache-manifest.e7521cb0a430d4c2c1af0527d94422df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab3c2649a41f9e090f74",
+    "url": "/my-app/static/css/main.d05b6c87.chunk.css"
+  },
+  {
+    "revision": "ab3c2649a41f9e090f74",
+    "url": "/my-app/static/js/main.ab3c2649.chunk.js"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b092478ada6f4c7edc87a97efea2c865",
+    "url": "/my-app/index.html"
+  }
+];
